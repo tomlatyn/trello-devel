28a0e8a31ffc3a792d6c74af4270de27093a79be
xlatyn <xlatyn@mendelu.cz>
2025-10-06 13:27:51 +0200

    Added logging

--- powerup/js/client.js
+++ powerup/js/client.js
@@ -1,10 +1,12 @@
 TrelloPowerUp.initialize({
   'card-back-section': function(t) {
+    console.log('card-back-section called');
     return Promise.all([
-      t.card('all'),  // Get all card fields
+      t.card('all'),  // Get all card fields including custom fields
       t.getAll()      // Get data stored by this power-up
     ])
     .then(function(results) {
+      console.log('Card data retrieved:', results);
       var cardData = results[0];
       var powerUpData = results[1];
 
@@ -15,13 +17,16 @@ TrelloPowerUp.initialize({
 
       var encodedData = encodeURIComponent(JSON.stringify(allData));
 
-      return {
+      var section = {
         title: 'Card Data',
         content: {
           type: 'iframe',
           url: t.signUrl('./card-back-section.html?data=' + encodedData)
         }
       };
+
+      console.log('Returning section:', section);
+      return section;
     })
     .catch(function(error) {
       console.error('Error in card-back-section:', error);
