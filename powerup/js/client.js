8cc8479bab6cfd60fa0eff4ce4e87be53fe5e16a
xlatyn <xlatyn@mendelu.cz>
2025-10-06 13:46:56 +0200

    Added icon

--- powerup/js/client.js
+++ powerup/js/client.js
@@ -19,6 +19,7 @@ TrelloPowerUp.initialize({
 
       var section = {
         title: 'Card Data',
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         content: {
           type: 'iframe',
           url: t.signUrl('./card-back-section.html?data=' + encodedData)
