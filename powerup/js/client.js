bd76afa99d18361f9f6a536e751fae91cd1128a3
xlatyn <xlatyn@mendelu.cz>
2025-10-07 17:36:44 +0200

    changed board buttons file

--- powerup/js/client.js
+++ powerup/js/client.js
@@ -14,15 +14,15 @@ TrelloPowerUp.initialize({
     }];
   },
   
-  // Keep your board button for later
+  // Board button - shows summary of all cards
   'board-buttons': function(t) {
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Calculate Hours',
+      text: 'Card Size Summary',
       callback: function(t) {
         return t.popup({
-          title: 'Board Hours Summary',
-          url: './hours-summary.html',
+          title: 'Board Card Size Summary',
+          url: './board-summary.html',
           height: 500
         });
       }
