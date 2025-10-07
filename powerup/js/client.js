933e7b437de1f00039d65ff51a718c3c39df9dfd
xlatyn <xlatyn@mendelu.cz>
2025-10-07 17:17:40 +0200

    Different approach

--- powerup/js/client.js
+++ powerup/js/client.js
@@ -1,4 +1,20 @@
 TrelloPowerUp.initialize({
+  // Add a card button to inspect individual cards
+  'card-buttons': function(t, options) {
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Inspect Card Data',
+      callback: function(t) {
+        return t.popup({
+          title: 'Card Data Inspector',
+          url: './card-inspector.html',
+          height: 500
+        });
+      }
+    }];
+  },
+  
+  // Keep your board button for later
   'board-buttons': function(t) {
     return [{
       icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
